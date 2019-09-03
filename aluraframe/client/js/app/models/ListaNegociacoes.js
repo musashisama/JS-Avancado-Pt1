46d109032b5692dc3c492f180c526b87567c5596
Paulo de Carvalho <48837659+musashisama@users.noreply.github.com>
2019-09-03 20:46:02 -0300

    Início da Parte 2.

--- aluraframe/client/js/app/models/ListaNegociacoes.js
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,18 +1,30 @@
 class ListaNegociacoes {
 
-    constructor(){
+    constructor(/* contexto,  */armadilha){
 
         this._negociacoes = [];
+        this._armadilha = armadilha;
+        //this._contexto = contexto;
     }
     
     adiciona(negociacao){
 
         this._negociacoes.push(negociacao);
+        this._armadilha(this);
+        // O Reflect recebe (1) Função a ser executada, (2) Contexto em que a função 1 será executada e (3) os parâmetros que serão passados pra função [array].
+        //Reflect.apply(this._armadilha,this._contexto,[this]);
     }
 
     get negociacoes() {
 
         return [].concat(this._negociacoes);
+    }
+
+    esvazia() {
+
+        this._negociacoes = [];
+        this._armadilha(this);
+       //Reflect.apply(this._armadilha,this._contexto,[this]);
     }
 
 
