a12623e7eddb1998f42aaa4512f4afc834e8096a
Paulo de Carvalho <48837659+musashisama@users.noreply.github.com>
2019-09-04 22:29:38 -0300

    Ajustes no proxy.

--- aluraframe/client/js/app/services/ProxyFactory.js
+++ aluraframe/client/js/app/services/ProxyFactory.js
@@ -12,9 +12,9 @@ class ProxyFactory {
 
                         console.log(`método '${prop}' interceptado`);
 
-                        Reflect.apply(target[prop], target, arguments);
-
-                        return acao(target);
+                        let retorno = Reflect.apply(target[prop], target, arguments);
+                        acao(target);
+                        return retorno;
                     }
             }
 
@@ -23,11 +23,9 @@ class ProxyFactory {
 
             set(target,prop,value,receiver){
                 
-                if(props.includes(prop)){
-                    target[prop] = value;
-                    acao(target);
-                }
-                return Reflect.set(target,prop,value,receiver);
+                let retorno = Reflect.set(target,prop,value,receiver);
+                if(props.includes(prop)) acao(target);                
+                return retorno;
             }
         });
     
